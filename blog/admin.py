04b31db750178785976038a920d5ef8154670fb3
fearfullymade01 <gegbeyemi.poise@gmail.com>
2026-01-12 17:54:05 +0000

    Fix PostAdmin: add created_on to list_display and list_filter, fix search_fields syntax

--- blog/admin.py
+++ blog/admin.py
@@ -13,9 +13,9 @@ class CommentInline(admin.TabularInline):
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content', 'created_on']
+    list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
